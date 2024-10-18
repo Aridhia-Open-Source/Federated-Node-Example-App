baf113e770c722b16593d04391c355d2f51aaf80
Riccardo Casula <riccardocasula@aridhia.net>
2024-10-18 16:30:30 +0100

    MS-SQL: Added missing imports, closed connection at the end

--- docker/app-test.R
+++ docker/app-test.R
@@ -1,5 +1,9 @@
-require("RODBC")
+library("RODBC")
+library("odbc")
+library("DBI")
+
 conn <- odbcDriverConnect(paste0('driver={ODBC Driver 18 for SQL Server};server=',Sys.getenv("PGHOST"), ';database=',Sys.getenv("PGDATABASE"),';uid=', Sys.getenv("PGUSER"), ';pwd=', Sys.getenv("PGPASSWORD"), ';TrustServerCertificate=Yes', sep=''))
-res <- sqlQuery(conn, 'select * from omop.person')
+res <- sqlQuery(conn, 'SELECT * FROM omop.person')
 print(res)
 write.csv(res, file='/mnt/data/average.csv', row.names=FALSE)
+odbcCloseAll()
