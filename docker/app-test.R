997d894555dd3f7b2dc341371ef6fb28b9159ad5
jananimurthy <janani.murthy@aridhia.com>
2024-10-18 12:36:13 +0100

    Changes for MS-SQL server.

--- docker/app-test.R
+++ docker/app-test.R
@@ -1,17 +1,6 @@
-require("RPostgreSQL")
-con <- dbConnect(
-    dbDriver("PostgreSQL"),
-    dbname = Sys.getenv("PGDATABASE"),
-    host = Sys.getenv("PGHOST"),
-    port = Sys.getenv("PGPORT"),
-    user = Sys.getenv("PGUSER"),
-    password = Sys.getenv("PGPASSWORD")
-)
-
-query <- Sys.getenv("QUERY", "SELECT AVG(speed) AS \"Average Speed\" FROM carspeed;")
-res <- dbSendQuery(con, query)
-avg <- fetch(res)
-print(avg)
-write.csv(avg, file='/mnt/data/average.csv', row.names=FALSE)
-dbClearResult(res)
-dbDisconnect(con)
+install.packages("RODBC")
+require("RODBC")
+dbhandle <- odbcDriverConnect(paste0('driver={SQL Server};server=',Sys.getenv("PGHOST"), ';database=',Sys.getenv("PGDATABASE"),';uid=', Sys.getenv("PGUSER"), ';pwd=', Sys.getenv("PGPASSWORD")))
+res <- sqlQuery(dbhandle, 'select * from omop.person')
+print(res)
+write.csv(res, file='/mnt/data/average.csv', row.names=FALSE) 
